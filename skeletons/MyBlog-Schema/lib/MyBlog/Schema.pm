6aec3f7a3be523e3165cfe82d92dfd68bb6d3e95
Jess Robinson <castaway@desert-island.me.uk>
2010-09-18 16:34:38 +0100

    Completed initial version of the MyBlog::Schema

--- skeletons/MyBlog-Schema/lib/MyBlog/Schema.pm
+++ skeletons/MyBlog-Schema/lib/MyBlog/Schema.pm
@@ -3,9 +3,15 @@ package MyBlog::Schema;
 use warnings;
 use strict;
 
+our $VERSION = '0.01';
+
+use base 'DBIx::Class::Schema';
+
+__PACKAGE__->load_namespaces();
+
 =head1 NAME
 
-MyBlog::Schema - The great new MyBlog::Schema!
+MyBlog::Schema - DBIx::Class Schema for my blogging project
 
 =head1 VERSION
 
@@ -13,83 +19,25 @@ Version 0.01
 
 =cut
 
-our $VERSION = '0.01';
-
-
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
+This module represents a database schema for a Blog, used in the
+DBIx::Class tutorial.
 
     use MyBlog::Schema;
 
-    my $foo = MyBlog::Schema->new();
+    my $schema = MyBlog::Schema->connect('dbi:SQLite:my.db');
     ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 SUBROUTINES/METHODS
-
-=head2 function1
-
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
 
 =head1 AUTHOR
 
 Jess Robinson, C<< <castaway at desert-island.me.uk> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-myblog-schema at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=MyBlog-Schema>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
 
 =head1 SUPPORT
 
 You can find documentation for this module with the perldoc command.
 
     perldoc MyBlog::Schema
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=MyBlog-Schema>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/MyBlog-Schema>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/MyBlog-Schema>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/MyBlog-Schema/>
-
-=back
-
 
 =head1 ACKNOWLEDGEMENTS
 
